5e12475d1c3356b0e0d75849a4351f28b1bee69b
System <none>
2017-07-06 13:19:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -24,9 +24,25 @@ app.get("/dateValues/:dateVal", function(req,res){
   // res.send('url found');
   // res.sendStatus(200);
   //console.log('correct url');
+  //get req for date
   var dateVal = req.params.dateVal;
-  
-  res.json({unix: dateVal});
+  //format the date
+  var dateFormat = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  if(isNaN(dateVal)){
+    var naturalDate = new Date(dateVal);
+    naturalDate = naturalDate.toLocaleDateString("en-us", dateFormat);
+    var unixDate = new Date(dateVal).getTime()/1000;
+  }
+  else{
+    var unixDate = dateVal;
+    var naturalDate = new Date(dateVal * 1000);
+    naturalDate = naturalDate.toLocaleDateString("en-us", dateFormat);
+  }
+  res.json({unix: unixDate, natural: naturalDate});
 });
 
 app.get("/dreams", function (request, response) {
